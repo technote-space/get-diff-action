b23a2c8cebedcbe506974d4529644415d5e0d5ca
Technote <technote.space@gmail.com>
2020-02-09 20:37:29 +0900

    feat: change target (#31)

--- src/constant.ts
+++ src/constant.ts
@@ -5,4 +5,5 @@ export const TARGET_EVENTS = {
 		'rerequested',
 		'synchronize',
 	],
+	'push': '*',
 };
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,14 @@
 export type FileDiffResult = { readonly insertions: number; readonly deletions: number; readonly lines: number };
 export type FileResult = { readonly file: string; readonly filterIgnored: boolean; readonly prefixMatched: boolean; readonly suffixMatched: boolean };
 export type DiffResult = FileDiffResult & FileResult;
+export type PullRequestParams = {
+	readonly base: {
+		readonly ref: string;
+	};
+}
+export type DiffInfo = {
+	readonly from: string;
+	readonly to: string;
+	readonly fromIsSha?: boolean;
+	readonly toIsSha?: boolean;
+}
