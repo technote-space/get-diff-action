e1ebfd7a25c57ee09963ca5f3c7294b943a5ecf2
Technote <technote.space@gmail.com>
2021-11-13 12:47:12 +0900

    chore: setup babel

--- jest.config.js
+++ jest.config.js
@@ -5,9 +5,8 @@ module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/*.test.ts'],
   testRunner: 'jest-circus/runner',
-  transform: {
-    '^.+\\.ts$': 'ts-jest',
-  },
   verbose: true,
   coverageDirectory: '<rootDir>/coverage',
+  preset: 'ts-jest/presets/js-with-babel',
+  transformIgnorePatterns: ['/node_modules/(?!multimatch|array-union|array-differ)'],
 };
