378162b2482491220ffcedfd5218359f3774aa6c
Technote <technote.space@gmail.com>
2022-01-05 16:10:47 +0900

    feat: add flag whether to get file diff (#173)
    BREAKING CHANGE: changed default behavior

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,6 @@
 export type FileDiffResult = Readonly<{insertions: number; deletions: number; lines: number}>;
 export type FileResult = Readonly<{file: string; filterIgnored: boolean; isMatched: boolean}>;
-export type DiffResult = FileDiffResult & FileResult;
+export type DiffResult = FileResult | (FileDiffResult & FileResult);
 export type PullRequestParams = Readonly<{
   base: {
     ref: string;
